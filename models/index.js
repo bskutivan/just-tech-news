e98cd4f75172b4d5ac1cc5b3553fc2325d863a33
BrandonKutivan <bskutivan@gmail.com>
2020-06-04 18:42:42 -0700

    added comments with routes and sequelize inclusions

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 //create associations
 User.hasMany(Post, {
@@ -17,6 +18,11 @@ User.belongsToMany(Post, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+
 Post.belongsToMany(User, {
     through: Vote,
     as: 'voted_posts',
@@ -31,6 +37,10 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 Vote.belongsTo(User, {
     foreignKey: 'user_id'
 });
@@ -39,5 +49,13 @@ Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote, Comment };
